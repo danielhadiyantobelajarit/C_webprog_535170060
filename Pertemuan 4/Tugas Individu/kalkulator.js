fe778b34020986e322d865211d35ac961e3b17e0
danielhadiyantobelajarit <danielhadiyanto308@gmail.com>
2020-09-28 21:08:35 +0700

    Tugas Individu

--- Pertemuan 4/Tugas Individu/kalkulator.js
+++ Pertemuan 4/Tugas Individu/kalkulator.js
@@ -1,8 +1,44 @@
-function tambah(bil1,bil2) {
-	var bil1 = document.getElementById('bil1').value;
-	var bil2 = document.getElementById('bil2').value;
-	var hasil = parseInt(bil1) + parseInt(bil2);
-	if(!isNaN(hasil)) {
-		document.getElementById('hasil').value = hasil;
+function cek() {
+	if(form.bil1.value == "" || form.bil2.value == ""){
+		alert("DATA KOSONG"); //jika ada angka yang kosong
+		exit;
 	}
+}
+
+function tambah() {
+	cek(); //panggil function cek
+	a = eval(form.bil1.value);
+	b = eval(form.bil2.value);
+	c = a+b
+	form.total.value = c;
+}
+
+function kurang() {
+	cek();
+	a = eval(form.bil1.value);
+	b = eval(form.bil2.value);
+	c = a-b
+	form.total.value = c;
+}
+
+function kali() {
+	cek();
+	a = eval(form.bil1.value);
+	b = eval(form.bil2.value);
+	c = a*b
+	form.total.value = c;
+}
+
+function bagi() {
+	cek();
+	a = eval(form.bil1.value);
+	b = eval(form.bil2.value);
+	c = a/b
+	form.total.value = c;
+}
+
+function cancel() {
+	form.bil1.value="";
+	form.bil2.value="";
+	form.total.value = "";
 }
